e44b06181740f1f25cfe2ed37a424e2af5eaf285
Tyuiu.NosyrevaUA@outlook.com <ulianosyreva752@gmail.com>
2023-11-19 01:50:54 +0500

    Task0

---
+++ Tyuiu.NosyrevaUA.Sprint5.Task0.V13.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.NosyrevaUA.Sprint5.Task0.V13.Lib
+{
+    public class DataService : ISprint5Task0V13
+
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
+            double z = -1.4 * Math.Pow(x, 3) + 2.3 * Math.Pow(x, 2) + 0.6 * x;
+            z = Math.Round(z, 3);
+            File.WriteAllText(path, Convert.ToString(z));
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.NosyrevaUA.Sprint5.Task0.V13.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.NosyrevaUA.Sprint5.Task0.V13.Lib;
+using System.IO;
+
+namespace Tyuiu.NosyrevaUA.Sprint5.Task0.V13.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidSaveToFileTextData()
+        {
+            string path = @"C:\Users\Юлия\source\repos\Tyuiu.NosyrevaUA.Sprint5\Tyuiu.NosyrevaUA.Sprint5.Task0.V13\bin\Debug\OutPutFileTask0.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool w = true;
+            Assert.AreEqual(w, fileExists);
+            
+        }
+    }
+}
