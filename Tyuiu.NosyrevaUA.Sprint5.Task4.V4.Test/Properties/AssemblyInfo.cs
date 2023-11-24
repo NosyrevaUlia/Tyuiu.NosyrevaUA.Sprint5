e562a6f1d75bb1ceb2807e3dbab79e58cfae9bea
Tyuiu.NosyrevaUA@outlook.com <ulianosyreva752@gmail.com>
2023-11-24 19:17:25 +0500

    Task4

---
+++ Tyuiu.NosyrevaUA.Sprint5.Task4.V4.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.NosyrevaUA.Sprint5.Task4.V4.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.NosyrevaUA.Sprint5.Task4.V4.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4b7a56f4-032e-4018-aebb-14f5aab3de2d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.NosyrevaUA.Sprint5.Task4.V4/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using Tyuiu.NosyrevaUA.Sprint5.Task4.V4.Lib;
+
+namespace Tyuiu.NosyrevaUA.Sprint5.Task4.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнила: Носырева Ю. А.| ИИПб-23-3 ";
+            
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Чтение данных из текстового файла                                 *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Выполнила: Носырева Юлия Алексеевна | ИИПб-23-3                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл С:DataSprint5InPutDataFileTask4V0.txt (файл взять из архива    *");
+            Console.WriteLine("* согласно вашему варианту. Создать папку в ручную С:DataSprint5 и        *");
+            Console.WriteLine("* скопировать в неё файл) в котором есть вещественное значение. Прочитать *");
+            Console.WriteLine("* значение из файла и подставить вместо Х в формуле. Вычислить значение   *");
+            Console.WriteLine("* по формуле (Полученное значение округлить до трёх знаков после запятой) *");
+            Console.WriteLine("* и вернуть полученный результат на консоль.                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = $@"C:DataSprint5\InPutDataFileTask4V4.txt";
+
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+            Console.ReadKey();
+
+        }
+    }
+}
